89cba64629a6b2fd85286ae4aa865826f6779aaf
RomanGolovinn <golovinroman.ev@gmail.com>
2025-07-21 19:11:19 +0300

    bot_init

---
+++ bot.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bot off")
